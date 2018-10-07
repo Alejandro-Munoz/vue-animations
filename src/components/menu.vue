d1240a5d0dba2451faf8149091ad2710bbda69fc
alejandro-munoz <alejandro.z.munoz@gmail.com>
2018-10-07 15:39:11 -0700

    menu component creation

---
+++ src/components/menu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div >
+        <h2>Menu</h2>
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+div{
+    width:20%;
+    height: 200px;
+    background:green;
+    color:white;
+}
+
+</style>
